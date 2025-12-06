5fc0c46599fc9e3365be574aef0cc7512285fb62
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-12-06 17:23:12 +0200

    feat: First release

--- src/main/kotlin/app/revanced/patcher/util/ProxyClassList.kt
+++
@@ -1,6 +0,0 @@
-package app.revanced.patcher.util
-
-import com.android.tools.smali.dexlib2.iface.ClassDef
-
-@Deprecated("Instead use PatchClasses", ReplaceWith("PatchClasses"))
-class ProxyClassList internal constructor(classes: MutableList<ClassDef>) : MutableList<ClassDef> by classes
