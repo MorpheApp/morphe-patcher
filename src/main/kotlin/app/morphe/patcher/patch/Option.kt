7fb5a18a4262d968456b74407bff8e120d4ccf62
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-01-16 18:58:24 +0100

    feat: Localized patch/option name, titles, descriptions

--- src/main/kotlin/app/morphe/patcher/patch/Option.kt
+++ src/main/kotlin/app/morphe/patcher/patch/Option.kt
@@ -1,5 +1,6 @@
 package app.morphe.patcher.patch
 
+import app.morphe.patcher.patch.Patch.Companion.sanitizeStringToPropertyKey
 import kotlin.reflect.KProperty
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
@@ -11,60 +12,113 @@ import kotlin.reflect.typeOf
  * @param key The key.
  * @param default The default value.
  * @param values Eligible option values mapped to a human-readable name.
+ * @param titleKey The key of the localized title. If none is provided then the automatic key of
+ *                 `${key}.title` is used if also declared in the localized patch string file.
  * @param title The title.
+ * @param descriptionKey The key of the localized title. If none is provided then the automatic key of
+ *                 `     ${key}.description` is used if also declared in the localized patch string file.
  * @param description A description.
  * @param required Whether the option is required.
  * @param type The type of the option value (to handle type erasure).
  * @param validator The function to validate the option value.
- *
- * @constructor Create a new [Option].
  */
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 class Option<T>
 @PublishedApi
-@Deprecated("Use the constructor with the name instead of a key instead.")
 internal constructor(
-    @Deprecated("Use the name property instead.")
     val key: String,
     val default: T? = null,
     val values: Map<String, T?>? = null,
-    @Deprecated("Use the name property instead.")
+    val titleKey: String? = null,
     val title: String? = null,
+    val descriptionKey: String? = null,
     val description: String? = null,
     val required: Boolean = false,
     val type: KType,
     val validator: Option<T>.(T?) -> Boolean = { true },
-)
-{
-    /**
-     * The name.
-     */
+) {
+    @Deprecated("Use the key property instead.")
     val name = key
 
     /**
-     * An option.
-     *
-     * @param T The value type of the option.
-     * @param name The name.
-     * @param default The default value.
-     * @param values Eligible option values mapped to a human-readable name.
-     * @param description A description.
-     * @param required Whether the option is required.
-     * @param type The type of the option value (to handle type erasure).
-     * @param validator The function to validate the option value.
-     *
-     * @constructor Create a new [Option].
+     * Required to lookup option keys by patch name, and to lookup resources from the mpp jar.
      */
-    @PublishedApi
-    internal constructor(
-        name: String,
-        default: T? = null,
-        values: Map<String, T?>? = null,
-        description: String? = null,
-        required: Boolean = false,
-        type: KType,
-        validator: Option<T>.(T?) -> Boolean = { true },
-    ) : this(name, default, values, name, description, required, type, validator)
+    internal var enclosingPatch: Patch<*>? = null
+
+    private fun getEnclosingPatch() : Patch<*> {
+        val patch = enclosingPatch
+        check(patch != null) {
+            "Option does not belong to any patch. Add the option to a patch to use it: \'$key\'"
+        }
+        return patch
+    }
+
+    private fun getPatchResourceLoader() = getEnclosingPatch().resourceClassLoader
+
+    private fun getLocalizationPropertySubKey(key: String) : String {
+        val parentKey = getEnclosingPatch().localizationPropertyKey ?: return key
+        return "$parentKey.$key"
+    }
+
+    /**
+     * @return The localized name using the patch string declarations.
+     * Falls back to non localized [key] if no localizations option name exists.
+     */
+    val titleLocalized: String?
+        get() {
+            // Backwards compatibility requires handling no resource loader.
+            val classLoader = getPatchResourceLoader() ?: return title
+
+            val stringKey = titleKey ?: getLocalizationPropertySubKey("$key.title")
+            val localized = PatchLocalization.getLocalizedString(
+                classLoader, stringKey
+            )
+
+            return localized ?: title
+        }
+
+    /**
+     * @return The localized description using patch string declarations.
+     * Falls back to non localized [description] if no localized patch description exist.
+     */
+    val descriptionLocalized: String?
+        get() {
+            // Backwards compatibility requires handling no resource loader.
+            val classLoader = getPatchResourceLoader() ?: return description
+            val stringKey = descriptionKey ?: getLocalizationPropertySubKey("$key.description")
+
+            val localized = PatchLocalization.getLocalizedString(
+                classLoader, stringKey
+            )
+
+            return localized ?: description
+        }
+
+    /**
+     * @return The localized description using patch string declarations.
+     * Falls back to non localized [description] if no localized patch description exist.
+     */
+    val valuesLocalized: Map<String, T?>?
+        get() {
+            if (values.isNullOrEmpty()) return values
+
+            // Backwards compatibility requires handling no resource loader.
+            val classLoader = getPatchResourceLoader() ?: return values
+            val localizedMap = mutableMapOf<String, T?>()
+
+            values.forEach { (valueKey, valueValue) ->
+                val stringKey = getLocalizationPropertySubKey(
+                    "$key.values." + sanitizeStringToPropertyKey(valueKey)
+                )
+
+                val localized = PatchLocalization.getLocalizedString(
+                    classLoader, stringKey
+                )
+                localizedMap[localized ?: valueKey] = valueValue
+            }
+
+            return localizedMap
+        }
 
     /**
      * The value of the [Option].
@@ -145,7 +199,7 @@ internal constructor(
 class Options internal constructor(
     private val options: Map<String, Option<*>>,
 ) : Map<String, Option<*>> by options {
-    internal constructor(options: Set<Option<*>>) : this(options.associateBy { it.name })
+    internal constructor(options: Set<Option<*>>) : this(options.associateBy { it.key })
 
     /**
      * Set an option's value.
@@ -892,14 +946,14 @@ sealed class OptionException(errorMessage: String) : Exception(errorMessage, nul
      *
      * @param value The value that failed validation.
      */
-    class ValueValidationException(value: Any?, option: Option<*>) : OptionException("The option value \"$value\" failed validation for ${option.name}")
+    class ValueValidationException(value: Any?, option: Option<*>) : OptionException("The option value \"$value\" failed validation for ${option.key}")
 
     /**
      * An exception thrown when a value is required but null was passed.
      *
      * @param option The [Option] that requires a value.
      */
-    class ValueRequiredException(option: Option<*>) : OptionException("The option ${option.name} requires a value, but the value was null")
+    class ValueRequiredException(option: Option<*>) : OptionException("The option ${option.key} requires a value, but the value was null")
 
     /**
      * An exception thrown when a [Option] is not found.
