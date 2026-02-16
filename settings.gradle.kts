80faf1726fbd9aaa5f244190d85764996a1b0fb9
Eric Ahn <wchill1337@gmail.com>
2026-02-15 19:41:55 -0800

    Remove apktool-lib from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "morphe-patcher"
 // Include Morphe forks of libraries as composite builds if they exist locally
 mapOf(
     "ARSCLib" to "com.github.REAndroid:arsclib",
-    "Apktool" to "app.morphe:apktool-lib",
     "multidexlib2" to "app.morphe:multidexlib",
 ).forEach { (libraryPath, libraryName) ->
     val libDir = file("../$libraryPath")
