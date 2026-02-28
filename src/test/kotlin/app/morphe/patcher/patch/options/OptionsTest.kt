e2525d92a37afe65173a43559ccb822a6accc9e1
Eric Ahn <wchill1337@gmail.com>
2026-02-28 01:48:31 -0800

    Remove unused code

--- src/test/kotlin/app/morphe/patcher/patch/options/OptionsTest.kt
+++ src/test/kotlin/app/morphe/patcher/patch/options/OptionsTest.kt
@@ -141,32 +141,5 @@ internal object OptionsTest {
         assertEquals(optionsTestPatch.options["external"].value, externalOption.value)
     }
 
-    @Test
-    fun `serializes and deserializes`() {
-        val options = mapOf(
-            "Test patch" to mapOf("key1" to "test", "key2" to false),
-        )
-
-        val patch = bytecodePatch("Test patch") {
-            stringOption("key1")
-            booleanOption("key2", true)
-        }
-        val duplicatePatch = bytecodePatch("Test patch") {
-            stringOption("key1")
-        }
-        val unnamedPatch = bytecodePatch {
-            booleanOption("key1")
-        }
-
-        setOf(patch, duplicatePatch, unnamedPatch).setOptions(options)
-
-        assert(patch.options["key1"].value == "test")
-        assert(patch.options["key2"].value == false)
-
-        assertEquals(patch.options["key1"].value, duplicatePatch.options["key1"].value)
-
-        assert(unnamedPatch.options["key1"].value == null)
-    }
-
     private fun options(block: Options.() -> Unit) = optionsTestPatch.options.let(block)
 }
