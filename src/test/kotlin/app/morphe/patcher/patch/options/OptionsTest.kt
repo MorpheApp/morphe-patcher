bf19fbf9b9f3d2b879faf8c5eeb8e64254f97437
Eric Ahn <wchill1337@gmail.com>
2026-02-28 01:36:47 -0800

    feat: Decouple morphe-library and morphe-patcher

--- src/test/kotlin/app/morphe/patcher/patch/options/OptionsTest.kt
+++ src/test/kotlin/app/morphe/patcher/patch/options/OptionsTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import kotlin.reflect.typeOf
 import kotlin.test.*
+import kotlin.text.get
 
 internal object OptionsTest {
     private val externalOption = stringOption("external", "default")
@@ -138,6 +139,33 @@ internal object OptionsTest {
     @Test
     fun `should allow getting the external option from the patch`() {
         assertEquals(optionsTestPatch.options["external"].value, externalOption.value)
+    }
+
+    @Test
+    fun `serializes and deserializes`() {
+        val options = mapOf(
+            "Test patch" to mapOf("key1" to "test", "key2" to false),
+        )
+
+        val patch = bytecodePatch("Test patch") {
+            stringOption("key1")
+            booleanOption("key2", true)
+        }
+        val duplicatePatch = bytecodePatch("Test patch") {
+            stringOption("key1")
+        }
+        val unnamedPatch = bytecodePatch {
+            booleanOption("key1")
+        }
+
+        setOf(patch, duplicatePatch, unnamedPatch).setOptions(options)
+
+        assert(patch.options["key1"].value == "test")
+        assert(patch.options["key2"].value == false)
+
+        assertEquals(patch.options["key1"].value, duplicatePatch.options["key1"].value)
+
+        assert(unnamedPatch.options["key1"].value == null)
     }
 
     private fun options(block: Options.() -> Unit) = optionsTestPatch.options.let(block)
