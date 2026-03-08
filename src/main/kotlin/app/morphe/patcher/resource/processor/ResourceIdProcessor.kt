59569cd53b0964c2fef96ad72f57200af923f4a0
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-03-08 10:17:36 +0100

    chore: Consolidate code, change some code to internal

--- src/main/kotlin/app/morphe/patcher/resource/processor/ResourceIdProcessor.kt
+++ src/main/kotlin/app/morphe/patcher/resource/processor/ResourceIdProcessor.kt
@@ -9,14 +9,13 @@ import app.morphe.patcher.patch.PatchException
 import app.morphe.patcher.resource.PublicXmlManager
 import app.morphe.patcher.resource.fileResourceTypes
 import app.morphe.patcher.resource.forEachAttribute
+import app.morphe.patcher.resource.inOrderTraverse
 import app.morphe.patcher.resource.parseXml
 import app.morphe.patcher.resource.resourceToTagOverrideMapping
 import app.morphe.patcher.util.Document
-import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.xmlpull.v1.XmlPullParser
 import java.io.File
-import java.util.*
 import java.util.logging.Logger
 
 internal class ResourceIdProcessor(
@@ -66,7 +65,7 @@ internal class ResourceIdProcessor(
     private fun processIdAndAttrDeclarations(file: File, idNode: Node): Set<String> {
         val createdIds = mutableSetOf<String>()
         Document(file).use { doc ->
-            iterativeTraverse(doc.documentElement) { element ->
+            doc.documentElement.inOrderTraverse { element ->
                 val idString = element.getAttribute("android:id")
                 if (idString.startsWith("@+id/")) {
                     logger.fine { "Adding $idString to ids.xml" }
@@ -138,27 +137,6 @@ internal class ResourceIdProcessor(
                     }
                 }
                 eventType = parser.next()
-            }
-        }
-    }
-
-    /**
-     * Iterative traversal of all Element nodes in the subtree starting at [root].
-     * Preserves original in-order behavior of inOrderTraverse.
-     */
-    private fun iterativeTraverse(root: Node, action: (Element) -> Unit) {
-        val stack = ArrayDeque<Element>()
-        if (root is Element) stack.add(root)
-
-        while (stack.isNotEmpty()) {
-            val elem = stack.removeLast()
-            action(elem)
-
-            // Add children to stack in reverse order to maintain order
-            val children = elem.childNodes
-            for (i in children.length - 1 downTo 0) {
-                val child = children.item(i)
-                if (child is Element) stack.add(child)
             }
         }
     }
