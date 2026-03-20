f6d36c0153f344960b0883b691b0a7131fdf2f13
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-03-20 17:06:26 +0100

    Comments

--- src/main/kotlin/app/morphe/patcher/logging/ArsclibLogger.kt
+++ src/main/kotlin/app/morphe/patcher/logging/ArsclibLogger.kt
@@ -11,9 +11,7 @@ internal class ArsclibLogger(
     private val logger: Logger = NoOpLogger,
 ) : APKLogger {
     override fun logMessage(msg: String) {
-        // We call logger.trace() here too. That way without verbose the terminal shows 1 line only
-        // WARNING: THIS PUTS ALL THE NORMAL MESSAGES FROM ARSCLIB INTO FINE TOO SINCE THIS IS THE ONLY PLACE THAT USES IT.
-        // IF YOU WANT TO USE THIS IN THE FUTURE, BEWARE OF USING THIS BLINDLY.
+        // Category all ARSCLib messages as trace to keep patching logs more tidy.
         logger.trace(msg)
     }
 
