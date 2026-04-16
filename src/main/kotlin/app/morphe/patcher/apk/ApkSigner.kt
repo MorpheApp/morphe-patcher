1e5a23d478d40fd521488c8d026c524ae10f5894
Eric Ahn <wchill1337@gmail.com>
2026-04-15 18:36:10 -0700

    chore: Dump api

--- src/main/kotlin/app/morphe/patcher/apk/ApkSigner.kt
+++ src/main/kotlin/app/morphe/patcher/apk/ApkSigner.kt
@@ -15,9 +15,15 @@ import java.io.File
 import java.io.IOException
 import java.io.InputStream
 import java.math.BigInteger
-import java.security.*
+import java.security.KeyPairGenerator
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.SecureRandom
+import java.security.Security
+import java.security.UnrecoverableKeyException
 import java.security.cert.X509Certificate
-import java.util.*
+import java.util.Date
+import java.util.Locale
 import java.util.logging.Logger
 
 /**
@@ -224,7 +230,7 @@ object ApkSigner {
     )
 
     class Signer internal constructor(private val signerBuilder: com.android.apksig.ApkSigner.Builder) {
-        fun signApk(inputApkFile: File, outputApkFile: File, verifyOutput: Boolean = false) {
+        fun signApk(inputApkFile: File, outputApkFile: File) {
             logger.info("Signing APK")
 
             signerBuilder.setInputApk(inputApkFile)?.setOutputApk(outputApkFile)?.build()?.sign()
