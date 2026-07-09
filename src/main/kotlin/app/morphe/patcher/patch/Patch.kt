ecb54a834c7d7db162271c3f1c3de5ee71bf0168
Eric Ahn <wchill1337@gmail.com>
2026-07-08 20:01:34 -0700

    fix: Ignore versioned classes from multi-release JARs

--- src/main/kotlin/app/morphe/patcher/patch/Patch.kt
+++ src/main/kotlin/app/morphe/patcher/patch/Patch.kt
@@ -746,7 +746,7 @@ sealed class PatchLoader(
         PatchLoader(
             patchesFiles,
             { file ->
-                JarFile(file).entries().toList().filter { it.name.endsWith(".class") }
+                JarFile(file).entries().toList().filter { it.name.endsWith(".class") && !it.name.startsWith("META-INF/versions/") }
                     .map { it.name.substringBeforeLast('.').replace('/', '.') }
             },
             URLClassLoader(patchesFiles.map { it.toURI().toURL() }.toTypedArray()),
@@ -816,8 +816,15 @@ sealed class PatchLoader(
          */
         private fun ClassLoader.loadPatches(binaryClassNamesByPatchesFile: Map<File, Set<String>>) =
             binaryClassNamesByPatchesFile.mapValues { (_, binaryClassNames) ->
-                binaryClassNames.asSequence().map {
-                    loadClass(it)
+                binaryClassNames.asSequence().mapNotNull {
+                    try {
+                        loadClass(it)
+                    } catch (e: NoClassDefFoundError) {
+                        // Can happen under certain conditions, like loading classes with no base version.
+                        // Should be caught by filtering out classes under META-INF, but check again here
+                        // just in case.
+                        null
+                    }
                 }.flatMap {
                     it.patchFields + it.patchMethods
                 }.filter {
